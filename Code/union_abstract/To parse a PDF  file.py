c6b100becf207f7c35750d6059a654bf7e9ace41
Kenny Hsu <tei1004@yahoo.com.tw>
2016-06-07 16:59:17 +0800

    parse pdf

---
+++ Code/union_abstract/To parse a PDF  file.py
@@ -0,0 +1,29 @@
+Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 20:32:19) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfpage import PDFTextExtractionNotAllowed
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfdevice import PDFDevice
+
+# Open a PDF file.
+fp = open('mypdf.pdf', 'rb')
+# Create a PDF parser object associated with the file object.
+parser = PDFParser(fp)
+# Create a PDF document object that stores the document structure.
+# Supply the password for initialization.
+document = PDFDocument(parser, password)
+# Check if the document allows text extraction. If not, abort.
+if not document.is_extractable:
+    raise PDFTextExtractionNotAllowed
+# Create a PDF resource manager object that stores shared resources.
+rsrcmgr = PDFResourceManager()
+# Create a PDF device object.
+device = PDFDevice(rsrcmgr)
+# Create a PDF interpreter object.
+interpreter = PDFPageInterpreter(rsrcmgr, device)
+# Process each page contained in the document.
+for page in PDFPage.create_pages(document):
+    interpreter.process_page(page)
---
+++ Code/union_abstract/obtaining Table.py
@@ -0,0 +1,22 @@
+Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 20:32:19) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> document=mendeley.document.create_from_file("1022e.pdf")
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    document=mendeley.document.create_from_file("1022e.pdf")
+NameError: name 'mendeley' is not defined
+>>> from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+
+# Open a PDF document.
+fp = open('mypdf.pdf', 'rb')
+parser = PDFParser(fp)
+document = PDFDocument(parser, password)
+
+# Get the outlines of the document.
+outlines = document.get_outlines()
+for (level,title,dest,a,se) in outlines:
+    print (level, title)
+    
+>>> 
