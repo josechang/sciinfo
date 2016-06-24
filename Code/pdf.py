8d4b589e01e560ad384199b198bed0f8120751b0
Wei <4A02C014@stust.edu.tw>
2016-06-24 09:40:32 +0800

    yee

--- Code/pdf.py
+++ Code/pdf.py
@@ -1,11 +1,11 @@
 import textract
 import os
-DATA_DIR = 'C:/Users/Wei/Desktop/mm'
+DATA_DIR = '/home/dd/nordron-sciinfo/Code/Wolverine_Webcrawler/pdfs'
 for filename in os.listdir(DATA_DIR):
  if(filename[-4:len(filename)] ==".pdf") :
   
-   text = textract.process('C:/Users/Wei/Desktop/mm/'+filename, method='pdfminer')
-   file_txt = 'C:/Users/Wei/Desktop/mm/'+filename.replace(".pdf",".txt")
+   text = textract.process('/home/dd/nordron-sciinfo/Code/Wolverine_Webcrawler/pdfs'+filename, method='pdfminer')
+   file_txt = '/home/dd/nordron-sciinfo/Code/Wolverine_Webcrawler/txt'+filename.replace(".pdf",".txt")
    xml=open(file_txt,'w')
    for i in xrange(len(text)):
         xml.write(text[i])
