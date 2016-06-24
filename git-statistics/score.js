0bd4793048aea8a967e7d1766737e8df9dde07f0
kurumalin <pallacanestro159@gmail.com>
2016-06-24 10:25:25 +0800

    calculate by js

--- git-statistics/score.js
+++ git-statistics/score.js
@@ -3,9 +3,16 @@ var num_row_total=tableTotal.rows.length;
 var num_col_total=tableTotal.rows[0].cells.length;
 
 function calculate(){
+	var weight=[0,0.18,0.06,0.36,0.1,0.1];
+	var count=0;
 	for (var r =1; r < num_row_total ; r++){
-		a=parseInt(tableTotal.rows[r].cells[num_col_total-1].innerHTML);
-		b=parseInt(tableTotal.rows[r].cells[num_col_total-2].children[0].value);
-		tableTotal.rows[r].cells[num_col_total-1].innerHTML=Math.round(a+0.2*b);
+		count=0;
+		for (var c= 1; c < num_col_total-2;c++){
+			a=parseInt(tableTotal.rows[r].cells[c].innerHTML);
+			count+=a*weight[c];
+		}
+		report=parseInt(tableTotal.rows[r].cells[num_col_total-2].children[0].value);
+		count+=report*0.2;
+		tableTotal.rows[r].cells[num_col_total-1].innerHTML=Math.round(count);
 	}
 }
