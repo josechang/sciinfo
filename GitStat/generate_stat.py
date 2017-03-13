33d5988f406d252c97b64fdfa393b5568ba0d0fb
Eric Lee <crazyeric890119@gmail.com>
2017-03-13 15:14:05 +0800

    Updated

--- GitStat/generate_stat.py
+++ GitStat/generate_stat.py
@@ -15,6 +15,8 @@ class Participant:
 	def __init__(self):
 		self.participant = []
 		self.semester = []
+		self.fake_commits = 0
+		self.large_commits = 0
 		self.author = []
 		self.commits = []
 		self.lines_inserted = []
@@ -40,14 +42,18 @@ class Participant:
 		self.words_inserted.append(words_inserted)
 		self.words_deleted.append(words_deleted)
 	
-	def remove_commit(self, commits, lines_inserted, lines_deleted, words_inserted, words_deleted):
-		self.commits[0] -= commits
+	def remove_commit(self, fake_commits, large_commits, lines_inserted, lines_deleted, words_inserted, words_deleted):
+		self.fake_commits += fake_commits
+		self.large_commits += large_commits
+		self.commits[0] -= large_commits+fake_commits
 		self.lines_inserted[0] -= lines_inserted
 		self.lines_deleted[0] -= lines_deleted
 		self.words_inserted[0] -= words_inserted
 		self.words_deleted[0] -= words_deleted
 
 	def __add__(self, other):
+		self.fake_commits += other.fake_commits
+		self.large_commits += other.large_commits
 		self.author.extend(other.author)
 		self.commits.extend(other.commits)
 		self.lines_inserted.extend(other.lines_inserted)
@@ -57,6 +63,8 @@ class Participant:
 		return self
 
 	def __iadd__(self, other):
+		self.fake_commits += other.fake_commits
+		self.large_commits += other.large_commits
 		self.author.extend(other.author)
 		self.commits.extend(other.commits)
 		self.lines_inserted.extend(other.lines_inserted)
@@ -68,24 +76,26 @@ class Participant:
 	def print_info(self):
 		print("Participant:\t%s"%(str(self.participant)))
 		print("Semester:\t%s"%(str(self.semester)))
+		print("Fake commits:\t%s"%(str(self.fake_commits)))
+		print("Large commits:\t%s"%(str(self.large_commits)))
 		print("Author:\t\t%s"%(str(self.author)))
 		print("Commits:\t%s"%(str(self.commits)))
-		print("lines_inserted:\t%s"%(str(self.lines_inserted)))
-		print("lines_deleted:\t%s"%(str(self.lines_deleted)))
-		print("words_inserted:\t%s"%(str(self.words_inserted)))
-		print("words_deleted:\t%s"%(str(self.words_deleted)))
+		print("Lines inserted:\t%s"%(str(self.lines_inserted)))
+		print("Lines deleted:\t%s"%(str(self.lines_deleted)))
+		print("Words inserted:\t%s"%(str(self.words_inserted)))
+		print("Words deleted:\t%s"%(str(self.words_deleted)))
 		print("")
 
 def print_Participants(arr):
 	print(">> Participants: ")
 	for i in range(len(arr)):
-			print("%3i  %i  %s(%s)"%(i, arr[i].semester, arr[i].participant, str(arr[i].author)))
+			print("%3d %d %-20s\t(%s)"%(i, arr[i].semester, arr[i].participant, str(arr[i].author)))
 	print("")
 
 def print_Authors(arr):
 	print(">> Authors Remain: ")
 	for i in range(len(arr)):
-                        print("%3i %s %s"%(i, str(arr[i].author), str(arr[i].commits)))
+                        print("%3d %-20s\t%s"%(i, str(arr[i].author), str(arr[i].commits)))
         print("")
 
 def remove_last(arr):
@@ -117,53 +127,83 @@ def updaterep():
 	p.wait()
 	print("")
 
-def get_authors():
-	cmd = ["git --git-dir=%s shortlog -sne HEAD"%(git_dir), "/usr/bin/awk 'BEGIN{FS=\"\t\"}{print $2,$1}'"]
-        author_commits = getdata(cmd)
-        for x in author_commits:
-                (aut, com) = x.split("    ")
-		aut = aut.split(" <")[0]
-		author = Participant()
-		author.add_commits(aut, int(com))
-		author_list.append(author)
-
 def get_lines_data():
 	for i in range(len(author_list)):
                 cmd=["git --git-dir=%s log --numstat --author='%s'"%(git_dir, author_list[i].author[0]), \
-		'grep "^[0-9]"', "awk '{inserted+=$1;deleted+=$2} END {print inserted,deleted}'"]
+			'grep "^[0-9]"', "awk '{inserted+=$1;deleted+=$2} END {print inserted,deleted}'"]
                 (ins, dlt)=getdata(cmd)[0].split(" ")
 		author_list[i].add_lines(int(ins), int(dlt))
 
 def get_words_data():
 	for i in range(len(author_list)):
                 cmd1 = ["git --git-dir=%s log -p --word-diff=porcelain --author='%s'"%(git_dir, author_list[i].author[0]), \
-		'grep "^+[^+]"', "awk '{count+= NF}END{if(count==NULL){print 0}else{print count}}'"]
+			'grep "^+[^+]"', "awk '{count+= NF}END{if(count==NULL){print 0}else{print count}}'"]
                 cmd2 = ["git --git-dir=%s log -p --word-diff=porcelain --author='%s'"%(git_dir, author_list[i].author[0]), \
-		'grep "^-[^-]"', "awk '{count+= NF}END{if(count==NULL){print 0}else{print count}}'"]
+			'grep "^-[^-]"', "awk '{count+= NF}END{if(count==NULL){print 0}else{print count}}'"]
 		author_list[i].add_words(int(getdata(cmd1)[0]), int(getdata(cmd2)[0]))
 
-def remove_fake_commits():
-	cmd = ["grep 'commits/' fake_commits.txt"]
-        f = getdata(cmd)
-	for i, c in enumerate(f):
-                f[i] = c.replace("\r","")
-        for i, url in enumerate(f):
-                (a, b) = url.split("commits/")
-                f[i] = b
-	for commit in f:
-                cmd_author = ["git --git-dir=%s log %s -n 1"%(git_dir, commit), "grep 'Author:'"]
-		cmd_del_word = ["git --git-dir=%s log -p --word-diff=porcelain %s -n 1"%(git_dir, commit), \
-		'grep "^-[^-]"', "awk '{count+= NF}END{if(count==NULL){print 0}else{print count}}'"]
-	        cmd_ins_word = ["git --git-dir=%s log -p --word-diff=porcelain %s -n 1"%(git_dir,commit), \
-		'grep "^+[^+]"', "awk '{count+= NF}END{if(count==NULL){print 0}else{print count}}'"]
-              	cmd_line = ["git --git-dir=%s log --numstat %s -n 1"%(git_dir, commit), \
-		'grep "^[0-9]"', "awk '{inserted+=$1;deleted+=$2} END {print inserted,deleted}'"]
-                (y, z) = getdata(cmd_line)[0].split(" ")
-                (x, aut) = getdata(cmd_author)[0].split("Author: ")
+def get_authors():
+	cmd = ["git --git-dir=%s shortlog -sne HEAD"%(git_dir), "/usr/bin/awk 'BEGIN{FS=\"\t\"}{print $2,$1}'"]
+        author_commits = getdata(cmd)
+	print("Getting author imformations...")
+        for x in author_commits:
+                (aut, com) = x.split("    ")
 		aut = aut.split(" <")[0]
-		for author in author_list:
-			if author.author == [aut]:
-				author.remove_commit(1, int(y), int(z), int(getdata(cmd_del_word)[0]), int(getdata(cmd_ins_word)[0]))
+		author = Participant()
+		author.add_commits(aut, int(com))
+		author_list.append(author)
+	get_lines_data()
+	get_words_data()
+	for x in author_list:
+		print("\t%-20s\t%d\t%d\t%d\t%d"%(str(x.author[0]), x.lines_inserted[0], x.lines_deleted[0], x.words_inserted[0], x.words_deleted[0]))
+	print("")
+
+def remove_fake_commits():
+	count = 0
+	threshold = 2000
+	cmd1 = ["git log --all", "grep '^commit '"]		
+	f = getdata(cmd1)
+	for i, c in enumerate(f):
+                c = c.replace("\r","")
+                c = c.replace(" ","")
+                (a, b) = c.split("commit")
+                f[i] = b
+
+	cmd2 = ["grep 'commits/' fake_commits.txt"]
+        g = getdata(cmd2)
+        for i, c in enumerate(g):
+                c = c.replace("\r","")
+                c = c.replace(" ","")
+		(a, b) = c.split("commits/")
+		g[i] = b
+
+	print("Checking fake commits...")
+	for commit in f:
+		cmd_author = ["git --git-dir=%s log %s -n 1"%(git_dir, commit), "grep 'Author:'"]
+                cmd_del_word = ["git --git-dir=%s log -p --word-diff=porcelain %s -n 1"%(git_dir, commit), \
+                        'grep "^-[^-]"', "awk '{count+= NF}END{if(count==NULL){print 0}else{print count}}'"]
+                cmd_ins_word = ["git --git-dir=%s log -p --word-diff=porcelain %s -n 1"%(git_dir,commit), \
+                        'grep "^+[^+]"', "awk '{count+= NF}END{if(count==NULL){print 0}else{print count}}'"]
+                cmd_line = ["git --git-dir=%s log --numstat %s -n 1"%(git_dir, commit), \
+                        'grep "^[0-9]"', "awk '{inserted+=$1;deleted+=$2} END {print inserted,deleted}'"]
+		ins_word = int(getdata(cmd_ins_word)[0])
+		del_word = int(getdata(cmd_del_word)[0])
+		if commit in g or ins_word > threshold or del_word > threshold:
+			count += 1
+                	(y, z) = getdata(cmd_line)[0].split(" ")
+                	(x, aut) = getdata(cmd_author)[0].split("Author: ")
+                	aut = aut.split(" <")[0]
+			if commit in g:
+				for author in author_list:
+					if author.author == [aut]:
+						author.remove_commit(1, 0, int(y), int(z), ins_word, del_word)
+				print("\t%s %-20s\t%d\t%d\t%d\t%d\t(FAKE)"%(str(commit), str(aut), int(y), int(z), ins_word, del_word))
+			else:
+				for author in author_list:
+                	                if author.author == [aut]:
+        	                                author.remove_commit(0, 1, int(y), int(z), ins_word, del_word)
+	                        print("\t%s %-20s\t%d\t%d\t%d\t%d"%(str(commit), str(aut), int(y), int(z), ins_word, del_word))
+	print("\tTotal fake commits: %d/%d\n"%(count, len(f)))
 
 def create_participant(semester, participant_name, author_name):
 	participant = Participant()
@@ -213,17 +253,121 @@ def generate_statistics():
 		return False
 	updaterep()
 	get_authors()
-	get_lines_data()
-	get_words_data()
 	remove_fake_commits()
 	match_participants()
 
-def create_html()
+def create_html():
+	print("Generating HTML file...")
 	script_dir = os.path.dirname(__file__)
 	relative_path = "html/index.html"
 	absolute_path = os.path.join(script_dir, relative_path)
 	with open(absolute_path, "w") as f:
 		format = '%Y-%m-%d %H:%M:%S'
 		f.write('<!DOCTYPE html>\n')
+		f.write('<html>\n')
+		f.write('  <head>\n')
+		f.write('    <meta charset="UTF-8">\n')
+		f.write('    <title>Statistics</title>\n')
+		f.write('    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">\n')
+		f.write('    <link href="css/table.css" rel="stylesheet" type="text/css" />\n')
+		f.write('    <link href="css/container.css" rel="stylesheet" type="text/css" />\n')
+		f.write('    <script type="text/javascript" src="score.js"></script>\n')
+		f.write('    <script type="text/javascript" src="jquery-latest.js"></script>\n')
+		f.write('    <script type="text/javascript" src="jquery.tablesorter.js"></script>\n')
+		f.write('    <script type="text/javascript">\n')
+		f.write('    $(document).ready(function() {\n')
+		f.write('      $("#statistic").tablesorter();\n')
+		f.write('      $("#score").tablesorter();\n')
+		f.write('      }\n')
+		f.write('    );\n')
+		f.write('    </script>\n')
+		f.write('  </head>\n')
+		f.write('  <body>\n')
+		f.write('    <div class="container" id="header">\n')
+		f.write('      <h1>Scientific Information Gathering and Processing for Engineering Research 2017</h1>\n')
+		f.write('      <p>Accessed at %s from git repository.</p>\n' \
+			%(datetime.datetime.now().strftime(format)))
+		f.write('    </div>\n')
+		f.write('\n')
+		f.write('    <div class="container">\n')
+		f.write('      <h2>Statistics of the Commits on Bitbucket</h2>\n')
+		f.write('      <table cellspacing="2" id="statistic" class="tablesorter">\n')
+		f.write('        <thead>\n')
+		f.write('        <tr>\n')
+		f.write('          <th>Authors</th>\n')
+		f.write('          <th>Semester</th>\n')
+		f.write('          <th>Fake Commits</th>\n')
+		f.write('          <th>Large Commits</th>\n')
+		f.write('          <th>Total Commits</th>\n')
+		f.write('          <th>Line Inserted</th>\n')
+		f.write('          <th>Line Deleted</th>\n')
+		f.write('          <th>Word Inserted</th>\n')
+		f.write('          <th>Word Deleted</th>\n')
+		f.write('          <th>Total GIT Score</th>\n')
+		f.write('        </tr>\n')
+		f.write('        </thead>\n')
+		f.write('        <tbody>\n')
+
+		for participant in participant_list:
+			f.write('          <tr>\n')
+			f.write('            <td>%s</td>\n'%(participant.participant))
+			f.write('            <td>%d</td>\n'%(participant.semester))
+			f.write('            <td>%d</td>\n'%(participant.fake_commits))
+			f.write('            <td>%d</td>\n'%(participant.large_commits))
+			f.write('            <td>%d</td>\n'%(sum(participant.commits)))
+			f.write('            <td>%d</td>\n'%(sum(participant.lines_inserted)))
+			f.write('            <td>%d</td>\n'%(sum(participant.lines_deleted)))
+			f.write('            <td>%d</td>\n'%(sum(participant.words_inserted)))
+			f.write('            <td>%d</td>\n'%(sum(participant.words_deleted)))
+			f.write('            <td>%d</td>\n')
+			f.write('          </tr>\n')
+
+		f.write('        </tbody>\n')
+		f.write('      </table>\n')
+		f.write('      <p id="total">Total authors: %d </p>\n'%(len(participant_list)))
+		f.write('    </div style="display:none">\n')
+		f.write('\n')
+		f.write('    <div class="container">\n')
+		f.write('      <h2>Total Score for the Course</h2>\n')
+		f.write('      <button class="btn" onclick="calculate()">Calculate Total Score</button>\n')
+		f.write('      <table cellspacing="2" id="score" class="tablesorter">\n')
+		f.write('        <thead>\n')
+		f.write('          <tr>\n')
+		f.write('            <th>Participants</th>\n')
+		f.write('            <th>Attendace at lecture</th>\n')
+		f.write('            <th>Attendance at daily scrum</th>\n')
+		f.write('            <th>GIT Score</th>\n')
+		f.write('            <th>Oral presentation</th>\n')
+		f.write('            <th>Quiz Score</th>\n')
+		f.write('            <th>Report Score</th>\n')
+		f.write('            <th>Total Score</th>\n')
+		f.write('          </tr>\n')
+		f.write('        </thead>\n')
+		f.write('        <tbody>\n')
+		
+		for participant in participant_list:
+                        f.write('          <tr>\n')
+                        f.write('            <td>%s</td>\n'%(participant.participant))
+                        f.write('            <td>%d</td>\n'%(participant.semester))
+                        f.write('            <td>%d</td>\n'%(0))
+                        f.write('            <td>%d</td>\n'%(0))
+                        f.write('            <td>%d</td>\n'%(0))
+                        f.write('            <td>%d</td>\n'%(0))
+			f.write('            <td>\n')
+			f.write('              <input type="text" value="85" onkeypress="return isNumberKey(event)" maxlength="3">\n')
+			f.write('            </td>\n')
+                        f.write('            <td>%d</td>\n'%(0))
+                        f.write('            <td>%s</td>\n')
+                        f.write('          </tr>\n')
+
+		f.write('        </tbody>\n')
+		f.write('      </table>\n')
+		f.write('    </div>\n')
+		f.write('\n')
+		f.write('  </body>\n')
+		f.write('</html>\n')	
+
+	print("")
 
 generate_statistics()
+create_html()
