58a66dbd6914ace8a0cb0c7b7021b8f1037f2947
ds lee <dickson.lee@nordlinglab.org>
2017-05-08 12:24:20 +0800

    add orginial similarity comparison functionality

---
+++ Code/Rainy/similarity/transformation.py
@@ -0,0 +1,33 @@
+import logging
+import os
+from gensim import corpora, models, similarities
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+'''
+transform documents from one vector representation into another.
+This process serves two goals:
+
+1. To bring out hidden structure in the corpus,
+discover relationships between words and use them to describe the documents in a new and (hopefully) more semantic way.
+
+2. To make the document representation more compact.
+This both improves efficiency (new representation consumes less resources) and efficacy (marginal data trends are ignored, noise-reduction).
+
+'''
+
+if (os.path.exists("../tmp/deerwester.dict")):
+    dictionary = corpora.Dictionary.load('../tmp/deerwester.dict')
+    corpus = corpora.MmCorpus('../tmp/deerwester.mm')
+    print("Used files generated from vecter_space_convert.py")
+else:
+    print("Please generate data set")
+
+tfidf = models.TfidfModel(corpus) # step 1 -- initialize a model
+
+corpus_tfidf = tfidf[corpus] # step 2 -- use the model to transform vectors
+
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2) # initialize an LSI transformation
+corpus_lsi = lsi[corpus_tfidf] # create a double wrapper over the original corpus: bow->tfidf->fold-in-lsi
+
+lsi.save('../tmp/model.lsi') # same for tfidf, lda, ...
