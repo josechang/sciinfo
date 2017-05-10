0156c53efabdae1bbb78617b2d0851fdb9a62eb1
Huan-wei Hsu <qqps4487@gmail.com>
2017-05-10 19:55:55 +0800

    Create a new django app.

--- Code/Django/src/mydjango/settings.py
+++ Code/Django/src/mydjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'SearchDB.apps.SearchdbConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
