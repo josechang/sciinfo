3540a01de0a19cec9500af380985cd115bc026b6
Huan-wei Hsu <qqps4487@gmail.com>
2017-05-15 17:16:22 +0800

    Simplify the model

--- Code/Django/src/SearchDB/models.py
+++ Code/Django/src/SearchDB/models.py
@@ -5,11 +5,8 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    filename = models.CharField(max_length = 64)
+    filename = models.CharField(max_length = 255)
     content = models.TextField()
-    title = models.CharField(max_length = 50)
-    author = models.CharField(max_length = 20)
-    abstract = models.TextField()
-    year = models.PositiveSmallIntegerField()
+    title = models.CharField(max_length = 255)
     def __str__(self):
         return self.filename
