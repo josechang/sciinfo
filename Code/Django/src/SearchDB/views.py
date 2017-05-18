0e668413d7908b1bd82040b69502033d27bbe69d
Huan-wei Hsu <qqps4487@gmail.com>
2017-05-18 20:34:00 +0800

    Modified views.py

--- Code/Django/src/SearchDB/views.py
+++ Code/Django/src/SearchDB/views.py
@@ -3,19 +3,23 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404, render_to_response
 from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 from .models import Article
 from .forms import TextForm
 
 # Create your views here.
 def get_text(request):
+
+    # if the search bar gets query
     if 'user_query' in request.GET:
+        # Access the database to do searching
+        article_all = Article.objects.all()
+        vector = SearchVector('content')
+        query = SearchQuery(request.GET['user_query'])
+
         return HttpResponse('Your search string is: '+request.GET['user_query'])
+
     else:
         return render_to_response('SearchDB/search.html', locals())
 
-def show_text(request):
-    if request.method == 'POST':
-        form = TextForm(request.POST)
-        if form.is_valid():
-            return render(request, 'SearchDB/result.html', {'form': form})
