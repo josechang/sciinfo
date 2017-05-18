ca074246abdbde4ebe07df5eaa7c0cc9b746f17e
ds lee <dickson.lee@nordlinglab.org>
2017-05-18 21:15:33 +0800

    add search function into jdango.

--- Code/Django/src/SearchDB/views.py
+++ Code/Django/src/SearchDB/views.py
@@ -8,6 +8,15 @@ from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 from .models import Article
 from .forms import TextForm
 
+import imp
+
+vc = imp.load_source('vector_space_convert', '../nordron-sciinfo/Code/Rainy/code/vector_space_convert.py')
+vc.MyClass()
+sim = imp.load_source('similarity', '../nordron-sciinfo/Code/Rainy/code/similarity.py')
+sim.MyClass()
+tf = imp.load_source('transformation', '../nordron-sciinfo/Code/Rainy/code/transformation.py')
+tf.MyClass()
+
 # Create your views here.
 def get_text(request):
 
@@ -18,8 +27,12 @@ def get_text(request):
         vector = SearchVector('content')
         query = SearchQuery(request.GET['user_query'])
 
+        # implement searching function and ranks
+        vc.vecter_space_convert(article_all)
+        tf.transformation()
+        rank_score = sim.similarity_compare(query)
+
         return HttpResponse('Your search string is: '+request.GET['user_query'])
 
     else:
         return render_to_response('SearchDB/search.html', locals())
-
