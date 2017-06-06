7cfe3e9745485a7c19880f6a9676eedcd2b965db
Rain Wu <Rain.Wu@nordlinglab.org>
2017-06-06 15:00:11 +0800

    extract part of subheading

--- Code/Rainy/code/extraction/test.py
+++ Code/Rainy/code/extraction/test.py
@@ -2,4 +2,10 @@ from pyPdf import PdfFileReader
 f = open('file_1.pdf', 'rb')
 p = PdfFileReader(f)
 o = p.getOutlines()
-print o
+outline = o[1][2][0]
+test = len(o)
+print outline
+print test
+#test =outline[0]
+#outline = outline["/Title"]
+#print outline
