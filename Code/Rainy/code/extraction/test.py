0841639e905963e5d02ffd6e9206ca2fbde60035
Rain Wu <Rain.Wu@nordlinglab.org>
2017-06-07 11:59:19 +0800

    tried to extract subheading

--- Code/Rainy/code/extraction/test.py
+++ Code/Rainy/code/extraction/test.py
@@ -1,11 +1,24 @@
 from pyPdf import PdfFileReader
+import numpy as np
 f = open('file_1.pdf', 'rb')
 p = PdfFileReader(f)
 o = p.getOutlines()
 outline = o[1][2][0]
-test = len(o)
-print outline
-print test
+test = np.array(o)
+#print outline
+#print test
 #test =outline[0]
 #outline = outline["/Title"]
 #print outline
+
+
+get_type = type(o[1])
+print get_type
+if get_type == list:
+    pass
+
+'''
+for i in o:
+    if type(i)=='list'
+        print "ok"
+'''
