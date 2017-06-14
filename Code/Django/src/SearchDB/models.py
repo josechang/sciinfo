4bc0af1dae81998dd0a2091b49b0a8ac910088e3
Huan-wei Hsu <qqps4487@gmail.com>
2017-06-14 11:56:58 +0800

    Add column in models.py for doi link.

--- Code/Django/src/SearchDB/models.py
+++ Code/Django/src/SearchDB/models.py
@@ -9,7 +9,7 @@ class Article(models.Model):
     filename = models.CharField(max_length = 255)
     content = models.TextField()
     title = models.CharField(max_length = 255)
-
+    doi = models.URLField(max_length = 128)
     def __str__(self):
         # Define the model name
         return self.filename
