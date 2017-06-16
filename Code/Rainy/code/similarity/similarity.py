374f545758a4d493ef18b0e8ca0aa9fba9857e3c
ds lee <dickson.lee@nordlinglab.org>
2017-06-16 20:37:31 +0800

    update change of similarity.py to return 2 decimal percentage.

--- Code/Rainy/code/similarity/similarity.py
+++ Code/Rainy/code/similarity/similarity.py
@@ -3,10 +3,15 @@ from gensim import corpora, models, similarities
 
 # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-def similarity_compare(query):
+def similarity_compare(query, txt, DictPath, mmPath, indexPath, filename):
+    dictionary = corpora.Dictionary.load(DictPath + filename + '.dict')
+    corpus = corpora.MmCorpus(mmPath + filename + '.mm') # comes from vecter_space_convert.py, "From strings to vectors"
 
-    dictionary = corpora.Dictionary.load('../../tmp/deerwester.dict')
-    corpus = corpora.MmCorpus('../../tmp/deerwester.mm') # comes from vecter_space_convert.py, "From strings to vectors"
+    '''
+    for i in range(0, 10):
+        print corpus[i]
+        print dictionary[i]
+    '''
 
     lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
 
@@ -15,11 +20,22 @@ def similarity_compare(query):
     vec_lsi = lsi[vec_bow] # convert the query to LSI space
 
     index = similarities.MatrixSimilarity(lsi[corpus]) # transform corpus to LSI space and index it
-    index.save('../../tmp/deerwester.index') # save similarity index
+    index.save(indexPath + filename + '.index') # save similarity index
 
     sims = index[vec_lsi] # perform a similarity query against the corpus
-    sims = sorted(enumerate(sims), key=lambda item: -item[1]) # calculate sorted similarity
+    sims_enu = []
+    # Transform tuple data structure to list data structure and replace id to exact filename
+    for i in range(0,len(sims)):
+        sims_enu.append([txt[i], sims[i]])
+    sims_enu = sorted(sims_enu, key=lambda item: -item[1]) # calculate sorted similarity
     # print(list(enumerate(sims)))
-    print(sims) # print sorted (document number, similarity score) 2-tuples
+    #print(sims) # print sorted (document number, similarity score) 2-tuples
 
-    return sims
+    # THRESHOLD
+    sims_thres_adjust = []
+    for element in sims_enu:
+        if element[1] > 0.5:
+            element[1] = round(element[1], 2)
+            sims_thres_adjust.append(element)
+
+    return sims_thres_adjust
