4190a50cead3b2841d6f9be38443073c0421cc28
dickson lee <dickson.lee@nordlinglab.org>
2017-06-22 04:52:09 +0800

    add /test in url.py

--- Code/Django/src/SearchDB/urls.py
+++ Code/Django/src/SearchDB/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from SearchDB.views import get_text, refreshDatabase
+# from.import views 
 
 app_name = 'SearchDB'
 urlpatterns = [
-    url(r'^$', get_text), # If the user input this url, the request will be sent to get_text function in views.py
+    url(r'^$', get_text), # If the user input this url, the request will be     sent to get_text function in views.py
     url(r'^update/$', refreshDatabase),
+    url(r '^test/$', chart, name = 'demo'),
 ]
