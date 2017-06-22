b582266ffa47d9d47018309145e9665b2c4c05d6
Huan-wei Hsu <qqps4487@gmail.com>
2017-06-22 14:47:28 +0800

    Fix charts problem.

--- Code/Django/src/SearchDB/urls.py
+++ Code/Django/src/SearchDB/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from SearchDB.views import get_text, refreshDatabase
+from SearchDB.views import get_text, refreshDatabase, chart
 # from.import views 
 
 app_name = 'SearchDB'
 urlpatterns = [
-    url(r'^$', get_text), # If the user input this url, the request will be     sent to get_text function in views.py
+    url(r'^$', get_text),
     url(r'^update/$', refreshDatabase),
-    url(r '^test/$', chart, name = 'demo'),
+    url(r'^test/$', chart, name = 'demo'),
 ]
