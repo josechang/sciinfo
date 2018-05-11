aab08d2b9d4149b98ed00f1270f92b1c20fa2fac
N16041391\user <112231tn@gmail.com>
2018-05-12 01:25:01 +0800

    'Do_not_Do_not_use_tab_to_coding'

--- Code/Django/src/SearchDB/views.py
+++ Code/Django/src/SearchDB/views.py
@@ -40,97 +40,97 @@ tmpName = 'deerwester'
 
 def get_text(request):
     # if the search bar gets query, redirect to the result page, using GET method
-	if 'q' in request.GET and 'check' in request.GET:     
-
-		# Access the database to do searching
-		article_all = Article.objects.all()
-		vector = SearchVector('content', weight='A')
-		query = SearchQuery(str(request.GET['q']))
-		uq = request.GET['q']
-		
-		uq_split = uq.split(' ')
-		for i in range(len(uq_split)):
-			uq_split[i] = str(uq_split[i])
-
-		for ii in range(len(uq_split)):
-			s = []
-			syn = vb.synonym(uq_split[ii], format='list')
-			try:
-				for j in syn:
-			
-					s.append(j)
-			except:
-				continue
-        
-		synonym = s
-		teststr = "Got the message"
-		
-        # implement searching function and ranks
-		yearsort = year_similarity_compare(uq, os.listdir(TXT_PATH), TMP_PATH, TMP_PATH, TMP_PATH, tmpName)
-		
-		year_simus = []
-		# create list and sorting similarity for each year 
-		for i in range(2018,1950,-1):
-			tmp = []
-			for j in range(0,len(yearsort)):
-				if i == yearsort[j][2]:
-					tmp.append([yearsort[j][0],yearsort[j][1],yearsort[j][0]])
-			tmp = sorted(tmp, key = lambda item: -item[1])
-			for k in range(0,len(tmp)):
-				result = Article.objects.get(filename = tmp[k][0])
-				year_simus.append([str(result.filename),tmp[k][1],tmp[k][2]])
-		
-		resultlist = []
-		for i in range(0,len(year_simus)):
-			result = Article.objects.get(filename = year_simus[i][0])   
-			resultlist.append([str(result.filename), year_simus[i][1]])
-		fig = chart(year_simus)
-        # return uq, resultlist to result.html
-		return render_to_response('SearchDB/result_test.html', {'uq': uq ,'resultlist': resultlist ,'fig': fig, 'teststr' : teststr, 'synonym': synonym})
-
-	elif 'q' in request.GET:
+    if 'q' in request.GET and 'check' in request.GET:     
 
         # Access the database to do searching
-		article_all = Article.objects.all()
-		vector = SearchVector('content', weight='A')
-		query = SearchQuery(str(request.GET['q']))
-		uq = request.GET['q']
+        article_all = Article.objects.all()
+        vector = SearchVector('content', weight='A')
+        query = SearchQuery(str(request.GET['q']))
+        uq = request.GET['q']
 
-		uq_split = uq.split(' ')
-		for i in range(len(uq_split)):
+        uq_split = uq.split(' ')
+        for i in range(len(uq_split)):
+            uq_split[i] = str(uq_split[i])
 
-			uq_split[i] = str(uq_split[i])
+        for ii in range(len(uq_split)):
+            s = []
+            syn = vb.synonym(uq_split[ii], format='list')
+            try:
+                for j in syn:
 
-		for ii in range(len(uq_split)):
-			s = []
-			syn = vb.synonym(uq_split[ii], format='list')
-			try:
-				for j in syn:
-			
-					s.append(j)
-			except:
-				continue
+                    s.append(j)
+            except:
+                continue
         
-		synonym = s
-		teststr = "Got the message"
+        synonym = s
+        teststr = "Got the message"
+
+        # implement searching function and ranks
+        yearsort = year_similarity_compare(uq, os.listdir(TXT_PATH), TMP_PATH, TMP_PATH, TMP_PATH, tmpName)
+
+        year_simus = []
+        # create list and sorting similarity for each year 
+        for i in range(2018,1950,-1):
+            tmp = []
+            for j in range(0,len(yearsort)):
+                if i == yearsort[j][2]:
+                    tmp.append([yearsort[j][0],yearsort[j][1],yearsort[j][0]])
+            tmp = sorted(tmp, key = lambda item: -item[1])
+            for k in range(0,len(tmp)):
+                result = Article.objects.get(filename = tmp[k][0])
+                year_simus.append([str(result.filename),tmp[k][1],tmp[k][2]])
+
+        resultlist = []
+        for i in range(0,len(year_simus)):
+            result = Article.objects.get(filename = year_simus[i][0])   
+            resultlist.append([str(result.filename), year_simus[i][1]])
+        fig = chart(year_simus)
+        # return uq, resultlist to result.html
+        return render_to_response('SearchDB/result_test.html', {'uq': uq ,'resultlist': resultlist ,'fig': fig, 'teststr' : teststr, 'synonym': synonym})
+
+    elif 'q' in request.GET:
+
+        # Access the database to do searching
+        article_all = Article.objects.all()
+        vector = SearchVector('content', weight='A')
+        query = SearchQuery(str(request.GET['q']))
+        uq = request.GET['q']
+
+        uq_split = uq.split(' ')
+        for i in range(len(uq_split)):
+
+            uq_split[i] = str(uq_split[i])
+
+        for ii in range(len(uq_split)):
+            s = []
+            syn = vb.synonym(uq_split[ii], format='list')
+            try:
+                for j in syn:
+
+                    s.append(j)
+            except:
+                continue
+        
+        synonym = s
+        teststr = "Got the message"
 
         
         # implement searching function and ranks
-		sims = similarity_compare(uq, os.listdir(TXT_PATH), TMP_PATH, TMP_PATH, TMP_PATH, tmpName)
-		resultlist = []
-		for i in range(0,len(sims)):
-			result = Article.objects.get(filename = sims[i][0])
-		with open(TXT_PATH + str(result.filename), "r") as f:
-				for line in f: pass
-			print line #this is the last line of the file
-			resultlist.append([str(result.filename), sims[i][1], line])
-		fig = chart(sims)
+        sims = similarity_compare(uq, os.listdir(TXT_PATH), TMP_PATH, TMP_PATH, TMP_PATH, tmpName)
+        resultlist = []
+        for i in range(0,len(sims)):
+            result = Article.objects.get(filename = sims[i][0])
+        with open(TXT_PATH + str(result.filename), "r") as f:
+                for line in f: pass
+            print line #this is the last line of the file
+            resultlist.append([str(result.filename), sims[i][1], line])
+        fig = chart(sims)
         # return uq, resultlist to result.html
-		return render_to_response('SearchDB/result.html', {'uq': uq ,'resultlist': resultlist ,'fig': fig , 'teststr' : teststr, 'synonym': synonym})
-		
+        return render_to_response('SearchDB/result.html', {'uq': uq ,'resultlist': resultlist ,'fig': fig , 'teststr' : teststr, 'synonym': synonym})
 
-	else:
-		return render_to_response('SearchDB/search.html')
+
+    else:
+        return render_to_response('SearchDB/search.html')
 
 def refreshDatabase(request):
     # Create a list with filenames in SQL database
@@ -164,7 +164,7 @@ def refreshDatabase(request):
             # Load pdf file, for title
             pdf_filename = fname.replace(".txt", ".pdf")
             t = title_extractor(PDF_PATH, pdf_filename)
-			# y = year_extractor(PDF_PATH, pdf_filename)
+            # y = year_extractor(PDF_PATH, pdf_filename)
             d = doi_extract(PDF_PATH, pdf_filename)
             Article.objects.create(filename=fname, content=f.read(), title=t, doi=d)
             f.close()
