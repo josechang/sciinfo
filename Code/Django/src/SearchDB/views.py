c0677fc6a7076d1742e548c88f663ca9772401f6
Nick Chen <fasionout@gmail.com>
2018-05-13 12:50:50 +0800

    Make a mistake for deleting other's code , add them back.

--- Code/Django/src/SearchDB/views.py
+++ Code/Django/src/SearchDB/views.py
@@ -106,10 +106,10 @@ def get_text(request):
             abstract.append([(read_file)])
 			 
             file_open.close()
-	    #with open(TXT_PATH + str(result.filename), "r") as f:
-    	        #for line in f: pass
-    		#print line #this is the last line of the file
-            resultlist.append([str(result.filename), sims[i][1]])
+            with open(TXT_PATH + str(result.filename), "r") as f:
+                for line in f: pass
+                print line #this is the last line of the file
+            resultlist.append([str(result.filename), sims[i][1],line])
         fig = chart(sims)
         # return uq, resultlist to result.html
         return render_to_response('SearchDB/result.html', {'uq': uq ,'resultlist': resultlist ,'fig': fig , 'abstract' : abstract})
