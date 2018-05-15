b741a727ac5c5c9772428dbf96ba8375a012e654
Nick Chen <fasionout@gmail.com>
2018-05-15 19:56:40 +0800

    update abstract.py

--- Code/Django/src/abstract.py
+++ Code/Django/src/abstract.py
@@ -1,22 +1,29 @@
+# -*- coding: utf-8 -*-
 import pdfx
 import io
 import os
+import shutil
+
+#shutil.rmtree("Article_abstract/")
 
 if not os.path.exists('Article_abstract/'):
     os.mkdir('Article_abstract/')
-
-DATA_DIR = "Article_pdf"
+'''for i in os.listdir('Article_abstract/'):
+    os.remove('Article_abstract/' + i)'''
+#os.remove('Article_abstract/file_1abstract.txt')
+DATA_DIR = "Article_pdf/"
 pdf_name = []
 for filename in os.listdir(DATA_DIR):
-  print(filename)
-  if(filename[-4:len(filename)] ==".pdf") :
+  if not filename.replace(".pdf","abstract.txt") in os.listdir('Article_abstract/'):
+    print(filename)
+    if(filename[-4:len(filename)].lower() ==".pdf"):
       
-    pdf_name.append(filename)
-    pdf = pdfx.PDFx("Article_pdf/" + filename)
-    text = pdf.get_text()
-    xml=io.open("Article_txt/" + filename.replace(".pdf",".txt"),'w' ,encoding = 'utf8')
-    xml.write(text)
-    xml.close()
+      pdf_name.append(filename)
+      pdf = pdfx.PDFx("Article_pdf/" + filename)
+      text = pdf.get_text()
+      xml=io.open("Article_txt/" + filename.replace(".pdf",".txt"),'w' ,encoding = 'utf8')
+      xml.write(text)
+      xml.close()
 
 
 def abstract_start(a):
@@ -59,9 +66,8 @@ def abstract_end(a):
 
 
 
-for filename in os.listdir("Article_pdf"):
-  filename_txt = filename.replace(".pdf",".txt")
-  myfile = io.open("Article_txt/" + filename_txt,encoding = 'utf8')
+for filename in os.listdir("Article_txt/"):
+  myfile = io.open("Article_txt/" + filename,encoding = 'utf8')
   a1=[]
 
   c=0
@@ -80,7 +86,7 @@ for filename in os.listdir("Article_pdf"):
        
 
 
-  ab=io.open("Article_abstract/" + filename_txt.replace(".txt","abstract.txt"),'w' , encoding = 'utf8')
+  ab=io.open("Article_abstract/" + filename.replace(".txt","abstract.txt"),'w' , encoding = 'utf8')
 
   for i in range(len(a1)):
     ab.write(a1[i])
