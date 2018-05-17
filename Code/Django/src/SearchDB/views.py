c598e3ba26554d65fcd76d2e906466d00e046dc4
Nick Chen <fasionout@gmail.com>
2018-05-17 17:16:54 +0800

    fix the chart bug

--- Code/Django/src/SearchDB/views.py
+++ Code/Django/src/SearchDB/views.py
@@ -242,15 +242,15 @@ def chart(article_info):
 # Initialize list for counting articles of different percentage
     tmp = [0, 0, 0, 0, 0]
     for element in article_info:
-        if element[1] >= 0.9:
+        if element[1] > 0.9:
             tmp[0] += 1
-        if element[1] >= 0.8 and element[1] <= 0.9:
+        if element[1] > 0.8 and element[1] <= 0.9:
             tmp[1] += 1
-        if element[1] >= 0.7 and element[1] <= 0.8:
+        if element[1] > 0.7 and element[1] <= 0.8:
             tmp[2] += 1
-        if element[1] >= 0.6 and element[1] <= 0.7:
+        if element[1] > 0.6 and element[1] <= 0.7:
             tmp[3] += 1
-        if element[1] >= 0.5 and element[1] <= 0.6:
+        if element[1] > 0.5 and element[1] <= 0.6:
             tmp[4] += 1
 
 # Create an object for the column2d chart using the FusionCharts class constructor
@@ -265,23 +265,23 @@ def chart(article_info):
         },
         "data": [
                 {
-                    "label": "90-100",
+                    "label": "91-100",
                     "value": tmp[0]
                 },
                 {
-                    "label": "80-90",
+                    "label": "81-90",
                     "value": tmp[1]
                 },
                 {
-                    "label": "70-80",
+                    "label": "71-80",
                     "value": tmp[2]
                 },
                 {
-                    "label": "60-70",
+                    "label": "61-70",
                     "value": tmp[3]
                 },
                 {
-                    "label": "50-60",
+                    "label": "51-60",
                     "value": tmp[4]
                 }
         ]
