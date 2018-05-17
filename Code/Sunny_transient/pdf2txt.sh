be1d0ae901f9999430bdec7d753bf47afe8282ba
SunnyChing <sunny567886@gmail.com>
2018-05-17 09:41:28 +0800

    Sunny Sprint1 udpate

--- Code/Sunny_transient/pdf2txt.sh
+++ Code/Sunny_transient/pdf2txt.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+########################################
+#  shell script for transform pdf to txt and count the # of words of articles
+# you can put this script anywhere, only be careful to the path
+#
+# Editor: Sunny Tseng
+
 DATA_DIR=~/nordron-sciinfo/Code/Django/src/Article_pdf/
-TXT_DIR=~/nordron-sciinfo/Code/Django/src/Article_txt
-PROGRAM_DIR=~/nordron-sciinfo/Code/Django/src/
+TXT_DIR=~/nordron-sciinfo/Code/Django/src/Article_txt1
+PROGRAM_DIR=~/nordron-sciinfo/Code/Sunny_transient
 
 
 for f in `find $DATA_DIR*.pdf`
@@ -9,7 +16,7 @@ do
     echo "Converting $(basename $f) "
     pdfx $f -t -o $TXT_DIR/$(echo $(basename $f) | cut -f 1 -d '.').txt
     #sleep 5s
-    python $PROGRAM_DIR/countWordnumber.py $TXT_DIR/$(echo $(basename $f) | cut -f 1 -d '.').txt
+    #python $PROGRAM_DIR/countWordnumber.py $TXT_DIR/$(echo $(basename $f) | cut -f 1 -d '.').txt
 done
 
 
