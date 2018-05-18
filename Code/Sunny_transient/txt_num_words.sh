70f903effd5dc5ce7a3464b80dae57c79b408ed0
SunnyChing <sunny567886@gmail.com>
2018-05-18 08:29:33 +0800

    sunny sprint1 update

--- Code/Sunny_transient/txt_num_words.sh
+++ Code/Sunny_transient/txt_num_words.sh
@@ -6,7 +6,7 @@
 #  I put the file in ~/django/src means to count the # of words of articles and add a line at the bottom of that article.
 # Editor: Sunny Tseng
 #
-######################################################
+#######################################################
 TXT_DIR=~/django_image/src/Article_txt/
 PROGaRAM_DIR=~/nordron-sciinfo/Code/Sunny_transient
 for f in `find $TXT_DIR*.txt`
